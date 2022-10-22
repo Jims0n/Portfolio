b9249032eeef2f24be99dcaad53b1b3427b469b8
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-10-22 17:49:30 +0100

    project page

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import BlogImg from "../asset/Screenshot 2022-10-22 171715.png"
+import "./Project.css"
 
 const Project = () => {
   return (
-    <div><Navbar /></div>
+    <div>
+    <Navbar />
+    <div className="blog">
+    <a href='https://okay-articlus.herokuapp.com/'>
+    <img  src={BlogImg}  alt="BlogImg "/>
+    </a>
+    
+
+    </div>
+    </div>
   )
 }
 
