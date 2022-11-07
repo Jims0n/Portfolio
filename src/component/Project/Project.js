4e1483632926a57156412a2337f94b816bc17bf6
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-11-07 16:38:11 +0100

    edit project folder

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -7,13 +7,7 @@ const Project = () => {
   return (
     <div>
     <Navbar />
-    {/* <div className="blog">
-    <a href='https://okay-articlus.herokuapp.com/'>
-    <img  src={BlogImg}  alt="BlogImg "/>
-    </a>
-    
 
-    </div> */}
     </div>
   )
 }
