df8350947c2ef535663ff84d1f725fe0b1dd2ea6
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-11-07 16:39:12 +0100

    edit project folder

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-import BlogImg from "../asset/Screenshot 2022-10-22 171715.png"
+
 import "./Project.css"
 
 const Project = () => {
