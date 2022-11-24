082694ff492da6eb9ee9281490417aebb34282e5
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-11-25 00:40:09 +0100

    project page

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import "./Project.scss"
+import img1 from "../asset/Screenshot 2022-10-22 171715.png";
+import img2 from "../asset/Screenshot 2022-11-24 232340.png";
+import img3 from "../asset/Screenshot 2022-11-24 234542.png";
 
-import "./Project.css"
+
 
 const Project = () => {
   return (
     <div>
     <Navbar />
+    <div className='pImages'>
+    <div>
+    <a href='https://okay-articlus.herokuapp.com/'>
+    <img src={img1} alt='project1'/>
+    </a>
+    </div>
+    <div>
+    <a href='https://github.com/Jimson-Botterfly/Todolist'>
+    <img src={img2} alt='project2'/>
+    </a>
 
+    </div>
+    <div>
+    <a href='https://github.com/Jimson-Botterfly/keeper-app'>
+    <img src={img3} alt='project3'/>
+
+    </a>
+
+    </div>
+    </div>
+    
     </div>
   )
 }
